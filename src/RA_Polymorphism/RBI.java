bf20fb4335b7f6ad99ad30f2cf2ae5920fb67f4b
pranaybolla07@gmail.com <Pranayboll07@gmail.com>
2021-11-24 23:52:55 +0530

    my commit on 24th

--- src/RA_Polymorphism/RBI.java
+++ src/RA_Polymorphism/RBI.java
@@ -2,7 +2,6 @@ package RA_Polymorphism;
 
 public class RBI {
 	
-	//adding a comment to test git pull command
 	
 	public void getHomeLoanROI(String bankName)
 	{
--- src/testNg_apache/ParameterizeTestingPractice.java
+++ src/testNg_apache/ParameterizeTestingPractice.java
@@ -1,7 +1,5 @@
 package testNg_apache;
 
-import java.sql.Driver;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
